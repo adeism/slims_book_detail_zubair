f68c43ee39e825789c9feb2a2f2d07c349a252b0
Ade Ismail Siregar <adeismailbox@gmail.com>
2023-06-13 14:09:58 +0700

    Update detail.inc.php

--- detail.inc.php
+++ detail.inc.php
@@ -19,7 +19,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * 
+ *  modified : added total number of books available and books borrowed by github.com/adeism  */
  */
 
 // be sure that this file not accessed directly
